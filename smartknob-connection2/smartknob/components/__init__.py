21b8b8b048d4a1bc7c245d9c823fefd858b0be5f
Thijs Elenbaas <thijs.elenbaas@philips.com>
2025-09-03 00:05:01 +0200

    Added reset connection (intermediate fix until we can we request different components)

--- smartknob-connection2/smartknob/components/__init__.py
+++ smartknob-connection2/smartknob/components/__init__.py
@@ -1,3 +1,4 @@
 from .multiple_choice import MultipleChoiceSession
+from .toggle import ToggleComponent
 
-__all__ = ["MultipleChoiceSession"]
+__all__ = ["MultipleChoiceSession", "ToggleComponent"]
